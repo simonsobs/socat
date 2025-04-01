68411c417f5f619c061f68b15527815ecf70ff93
Jack Orlowski-Scherer <jackorlowskischerer@gmail.com>
2025-04-01 17:04:26 -0400

    feat: adding api tests for astroquery services. Known issue: adding source by name test fails

--- tests/test_api.py
+++ tests/test_api.py
@@ -103,7 +103,94 @@ def test_bad_id(client):
         response.raise_for_status()
 
     # TODO: should move to a different func
+    # TODO: Is this doing anything?
     response = client.post(
         "api/v1/source/box",
         json={"ra_min": 1, "ra_max": 0, "dec_min": 1, "dec_max": 0},
     )
+
+
+###################################################
+#              Astroquery Tests                   #
+###################################################
+
+
+def test_add_and_remove_service(client):
+    response = client.put(
+        "api/v1/service/new", json={"name": "Simbad", "config": "test"}
+    )
+
+    id = response.json()["id"]
+    assert response.status_code == 200
+
+    response = client.get("api/v1/service/{}".format(id))
+
+    assert response.status_code == 200
+    assert response.json()["name"] == "Simbad"
+    assert response.json()["config"] == "test"
+
+    response = client.get(
+        "api/v1/service/?service_name={}".format(response.json()["name"])
+    )
+
+    # There can be more than one source with the same name, so can't check anything else
+    assert response.status_code == 200
+
+    response = client.delete("api/v1/service/{}".format(id))
+
+    assert response.status_code == 200
+
+    response = client.get("api/v1/service/{}".format(id))
+
+    assert (
+        response.status_code == 404
+    )  # ID should be deleted, make sure we don't find it again
+
+
+def test_update_service(client):
+    response = client.put(
+        "api/v1/service/new", json={"name": "Simbad", "config": "test"}
+    )
+
+    id = response.json()["id"]
+    assert response.status_code == 200
+
+    response = client.post(
+        "api/v1/service/{}".format(id), json={"name": "VizieR", "config": "test2"}
+    )
+
+    assert response.status_code == 200
+    assert response.json()["id"] == id
+    assert response.json()["name"] == "VizieR"
+    assert response.json()["config"] == "test2"
+
+    response = client.delete("api/v1/service/{}".format(id))
+    assert response.status_code == 200
+
+
+def test_add_source_by_name(client):
+    response = client.put(
+        "api/v1/source/new?name={}&astroquery_service={}".format("m1", "Simbad"),
+        json={"requested_params": ["ra", "dec"]},
+    )
+
+    print(response.json())
+    id = response.json()["id"]
+    assert response.status_code == 200
+
+    response = client.get("api/v1/source/{}".format(id))
+
+    assert response.status_code == 200
+    assert response.json()["ra"] == 83.6324
+    assert response.json()["dec"] == 22.0174
+    assert response.json()["name"] == "m1"
+
+    response = client.delete("api/v1/source/{}".format(id))
+
+    assert response.status_code == 200
+
+    response = client.get("api/v1/source/{}".format(id))
+
+    assert (
+        response.status_code == 404
+    )  # ID should be deleted, make sure we don't find it again
