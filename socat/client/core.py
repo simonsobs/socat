29802be33ad828ac44c884f308d0fbcfa08abeaa
Jack Orlowski-Scherer <jackorlowskischerer@gmail.com>
2025-04-03 15:04:36 -0400

    feat: no covers:

--- socat/client/core.py
+++ socat/client/core.py
@@ -1,5 +1,6 @@
 """
 Abstract client class that must be implemented by both the mock and the 'real'
+Note none of the returns should ever be actually returned so no-covered
 client.
 """
 
@@ -16,7 +17,7 @@ class ClientBase(ABC):
         """
         Create a new source in the catlaog.
         """
-        return
+        return  # pragma: no cover
 
     @abstractmethod
     def create_name(
@@ -25,7 +26,7 @@ class ClientBase(ABC):
         """
         Create a new source in the catalog by name.
         """
-        return
+        return  # pragma: no cover
 
     @abstractmethod
     def get_box(
@@ -34,14 +35,14 @@ class ClientBase(ABC):
         """
         Get all sources within a box on the sky.
         """
-        return []
+        return []  # pragma: no cover
 
     @abstractmethod
     def get_source(self, *, id: int) -> ExtragalacticSource | None:
         """
         Get information about a specific source. If the source is not found, we return None.
         """
-        return None
+        return None  # pragma: no cover
 
     @abstractmethod
     def update_source(
@@ -55,14 +56,14 @@ class ClientBase(ABC):
         """
         Update a source. If the source is updated, return its new value. Else, return None.
         """
-        return None
+        return None  # pragma: no cover
 
     @abstractmethod
     def delete_source(self, *, id: int) -> None:
         """
         Delete a source from the catalog.
         """
-        return None
+        return None  # pragma: no cover
 
 
 class AstroqueryClientBase(ABC):
@@ -71,21 +72,21 @@ class AstroqueryClientBase(ABC):
         """
         Create a new service in the catalog.
         """
-        return
+        return  # pragma: no cover
 
     @abstractmethod
     def get_service(self, *, id: int) -> AstroqueryService | None:
         """
         Get information about a specific service. If the service is not found, we return None.
         """
-        return None
+        return None  # pragma: no cover
 
     @abstractmethod
     def get_service_name(self, *, name: str) -> list[AstroqueryService] | None:
         """
         Get information about a specific service by name. If the service is not found, we return None.
         """
-        return []
+        return []  # pragma: no cover
 
     @abstractmethod
     def update_service(
@@ -98,11 +99,11 @@ class AstroqueryClientBase(ABC):
         """
         Update a service. If the service is updated, return its new value. Else, return None.
         """
-        return None
+        return None  # pragma: no cover
 
     @abstractmethod
     def delete_service(self, *, id: int) -> None:
         """
         Delete a service from the catalog.
         """
-        return None
+        return None  # pragma: no cover
