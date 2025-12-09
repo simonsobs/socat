e7a3bab6bb1f1620c5f079c6b4ee51032d00231e
Jack Orlowski-Scherer <jackorlowskischerer@gmail.com>
2025-12-09 12:01:36 -0500

    feat: database classes for solar system and ephem

--- socat/database.py
+++ socat/database.py
@@ -109,7 +109,6 @@ class ExtragalacticSourceTable(SQLModel, table=True):
     ra_deg: float = Field(nullable=False)
     dec_deg: float = Field(nullable=False)
     flux_mJy: float | None = Field(nullable=True)
-
     name: str = Field(index=True, nullable=True)
 
     def to_model(self) -> ExtragalacticSource:
@@ -132,7 +131,147 @@ class ExtragalacticSourceTable(SQLModel, table=True):
         )
 
 
-ALL_TABLES = [ExtragalacticSourceTable, AstroqueryServiceTable]
+class SolarSystemSource(BaseModel):
+    """
+    A Solar system source.
+
+    Attributes
+    ----------
+    id : int
+        Internal SO ID of source
+    MPC_id : int | None
+        Minor Planet Center ID of ephem.
+    name : str
+        Name of source
+
+    """
+
+    id: int
+    MPC_id: int | None
+    name: str
+
+
+class SolarSystemTable(SolarSystemSource, SQLModel, table=True):
+    """
+    An solar system source. This is the table model
+    providing SQLModel functionality. You can export a base model, for example
+    for responding to a query with using the `to_model` method.
+    """
+
+    __tablename__ = "solarsystem_sources"
+
+    id: int = Field(primary_key=True)
+    MPC_id: int | None = Field(index=True, nullable=True)
+    name: str = Field(index=True, nullable=False)
+
+    def to_model(self) -> SolarSystemSource:
+        """
+        Return an Solar System source from table.
+
+        Returns
+        -------
+        SolarSystemSource : SolarSystemSource
+            Source corresponding to this id.
+        """
+        return SolarSystemSource(
+            id=self.id,
+            MPC_id=self.MPC_id,
+            name=self.name,
+        )
+
+
+class SolarSystemEphem(BaseModel):
+    """
+    A Solar system source at a given time.
+
+    Attributes
+    ----------
+    id : int
+        ID of ephem.
+    obj_id :int
+        Internal SO ID of source
+    MPC_id : int | None
+        MPC ID of source
+    name : str
+        Name of source
+    time : int
+        Time of source ephem, unix time
+    position : AstroPydanticICRS
+        Position of source in ICRS coordinates
+
+    """
+
+    id: int
+    obj_id: int
+    MPC_id: int | None
+    name: str
+    time: int
+    position: AstroPydanticQuantity
+    flux: AstroPydanticQuantity | None = None
+
+
+class SolarSystemEphemTable(SQLModel, table=True):
+    """
+    A Solar system source at a given time. This is the table model
+    providing SQLModel functionality. You can export a base model, for example
+    for responding to a query with using the `to_model` method.
+    """
+
+    __tablename__ = "solarsystem_ephem"
+
+    id: int = Field(primary_key=True)
+    obj_id: int = Field(
+        foreign_key="solarsystem_sources.id",
+        nullable=False,
+        ondelete="CASCADE",
+        onupdate="CASCADE",
+    )
+    MPC_id: int | None = Field(
+        foreign_key="solarsystem_sources.MPC_id",
+        nullable=True,
+        ondelete="CASCADE",
+        onupdate="CASCADE",
+    )
+    name: int = Field(
+        foreign_key="solarsystem_sources.name",
+        nullable=False,
+        ondelete="CASCADE",
+        onupdate="CASCADE",
+    )
+    time: int
+    ra_deg: float = Field(nullable=False)
+    dec_deg: float = Field(nullable=False)
+    flux_mJy: float | None = Field(nullable=True)
+
+    def to_model(self) -> SolarSystemEphem:
+        """
+        Return an solar system ephem from table.
+
+        Returns
+        -------
+        SolarSystemEphem : SolarSystemEphem
+            Source corresponding to this id at this time.
+        """
+        flux = self.flux_mJy
+        if self.flux_mJy is not None:
+            flux *= u.mJy
+        return SolarSystemEphem(
+            id=self.id,
+            obj_id=self.obj_id,
+            MPC_id=self.MPC_id,
+            name=self.name,
+            time=self.time,
+            position=ICRS(ra=self.ra_deg * u.deg, dec=self.dec_deg * u.deg),
+            flux=self.flux_mJy,
+        )
+
+
+ALL_TABLES = [
+    ExtragalacticSourceTable,
+    AstroqueryServiceTable,
+    SolarSystemTable,
+    SolarSystemEphemTable,
+]
 
 async_engine = create_async_engine(settings.database_url, echo=True, future=True)
 
