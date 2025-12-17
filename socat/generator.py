0f6b7da5ec06721f42d35962ebe3f2d7047e3e84
Jack Orlowski-Scherer <jackorlowskischerer@gmail.com>
2025-12-17 13:19:29 -0500

    feat: adding generator class for geting source locationg

--- socat/generator.py
+++ socat/generator.py
@@ -1,6 +1,8 @@
-from functools import cached_property
+from functools import lru_cache
 
 import numpy as np
+from astropy.coordinates import ICRS
+from astropy.units import Quantity
 from scipy.interpolate import make_interp_spline
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -14,21 +16,95 @@ class SourceGenerator:
         source: ExtragalacticSource | SolarSystemSource,
         t_min: int,
         t_max: int,
-        session: AsyncSession,
     ):
         self.source = source
+        self.t_min = t_min
+        self.t_max = t_max
+        self.interp = None
+
+    async def init_interp(self, session: AsyncSession) -> None:
+        """
+        Initialize the interpolator object.
+        If our source type is a RegisteredFixedSource, then
+        interp will always just return the same ra/dec/flux
+        (Recall that the RegisteredFixedSource.flux is
+        a fixed estimate and not the light curve). If
+        the soure type is SolarSystemSource, then linear
+        interp ra/dec/flux over the requested time range.
+
+        Parameters
+        ----------
+        session : AsyncSession
+            Session to use for querrying
+
+        Returns
+        -------
+        None
+        """
         if type(self.source) is ExtragalacticSource:
-            self.interp = lambda x: self.source.position
+            self.ra_unit = self.source.position.ra.unit
+            self.dec_unit = self.source.position.dec.unit
+            self.flux_unit = self.source.flux.unit
+            self.interp = lambda _: (
+                self.source.position.ra.value,
+                self.source.position.dec.value,
+                self.source.flux.value,
+            )
 
         elif type(self.source) is SolarSystemSource:
-            ephems = get_ephem_points(t_min=t_min, t_max=t_max, session=session)
+            ephems = await get_ephem_points(
+                self.source, t_min=self.t_min, t_max=self.t_max, session=session
+            )
             x = np.zeros(len(ephems))
             y = np.zeros((len(ephems), 3))
             for i, ephem in enumerate(ephems):
                 x[i] = ephem.time
-                y[i] = ephem.position.ra.value, ephem.position.dec.value, ephem.flux
+                y[i] = (
+                    ephem.position.ra.value,
+                    ephem.position.dec.value,
+                    ephem.flux.value,
+                )
+
+            self.ra_unit = ephem.position.ra.unit  # This assumes all ephem points have same units but this should probably be enforced upstream anyway.
+            self.dec_unit = ephem.position.dec.unit
+            self.flux_unit = ephem.flux.unit
             self.interp = make_interp_spline(x, y, k=1)
 
-    @cached_property
-    def at_time(self, t):
-        return self.interp(t)
+    @lru_cache(maxsize=128)  # This can cause memory leaks so we might not want it
+    def at_time(self, t: int) -> tuple[ICRS, Quantity]:
+        """
+        Get the ra/dec/flux of the source at the requested time.
+
+        Parameters
+        ----------
+        t : int
+            Time to get ra/dec/flux at
+
+        Returns
+        -------
+        (position, flux) : tuple[ICRS, Quantity]
+            Interpolated ra/dec/flux in the same units as self.source
+
+        Raises
+        ------
+        RuntimeError
+            If interp is not initialized.
+        ValueError
+            If t is outside t bounds.
+        """
+        if self.interp is None:
+            raise RuntimeError(
+                "Error: interp must be initialized first. Have you run init_interp?"
+            )
+        if t < self.t_min or self.t_max < t:
+            raise ValueError(
+                "Error, requested t={} outside initialized bounds {}-{}".format(
+                    t, self.t_min, self.t_max
+                )
+            )
+
+        ra, dec, flux = self.interp(t)
+        position = ICRS(ra=ra * self.ra_unit, dec=dec * self.dec_unit)
+        flux = flux * self.flux_unit
+
+        return (position, flux)
--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,16 +1,25 @@
 import astropy.units as u
+import numpy as np
 import pytest
 from astropy.coordinates import ICRS
 
-from socat import core
+from socat import core, generator
 
 
 @pytest.mark.asyncio
-async def test_add_and_retrieve(database_async_sessionmaker):
+async def test_gen(database_async_sessionmaker):
     t_min = 12345700
     t_max = 12345900
     async with database_async_sessionmaker() as session:
         sso = await core.create_sso(name="Davida", MPC_id=511, session=session)
+
+        position = ICRS(1 * u.deg, 1 * u.deg)
+        source = await core.create_source(
+            position,
+            session=session,
+            name="mySrc",
+            flux=1.5 * u.mJy,
+        )
 
         for i in range(10):
             position = ICRS(i * u.deg, 1.5 * i * u.deg)
@@ -32,6 +41,36 @@ async def test_add_and_retrieve(database_async_sessionmaker):
         assert t_min <= ephem.time
         assert ephem.time <= t_max
     assert len(ephem_list) == 3
+
+    async with database_async_sessionmaker() as session:
+        gen = generator.SourceGenerator(source, 123456789, 987654321)
+        await gen.init_interp(session=session)
+
+    position, flux = gen.at_time(t=234567891)
+
+    assert position.ra.value == 1
+    assert position.dec.value == 1
+    assert flux.value == 1.5
+
+    async with database_async_sessionmaker() as session:
+        gen = generator.SourceGenerator(sso, t_min=t_min, t_max=t_max)
+        await gen.init_interp(session=session)
+
+    position, flux = gen.at_time(12345850)
+
+    assert position.ra.value == 2.5
+    assert position.dec.value == 3.75
+    assert np.isclose(flux.value, 3.1)
+
+    # Check asking out of bounds doesn't work
+    with pytest.raises(ValueError):
+        gen.at_time(t_max + 100)
+
+    # Check not initializing interp raises error
+    with pytest.raises(RuntimeError):
+        async with database_async_sessionmaker() as session:
+            gen = generator.SourceGenerator(sso, t_min=t_min, t_max=t_max)
+        gen.at_time(12345850)
 
     async with database_async_sessionmaker() as session:
         await core.delete_sso(sso.sso_id, session=session)
